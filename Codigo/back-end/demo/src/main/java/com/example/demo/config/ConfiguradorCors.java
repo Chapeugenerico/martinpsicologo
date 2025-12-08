5cfe76ce5a6ec01922957dada523cc95baa93dd6
Chapeugenerico <anime.fan2045@gmail.com>
2025-12-08 20:30:33 -0300

    cors nome corrigido

--- Codigo/back-end/demo/src/main/java/com/example/demo/config/ConfiguradorCors.java
+++ Codigo/back-end/demo/src/main/java/com/example/demo/config/ConfiguradorCors.java
@@ -6,10 +6,10 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
+public class ConfiguradorCors {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer Configurador() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
